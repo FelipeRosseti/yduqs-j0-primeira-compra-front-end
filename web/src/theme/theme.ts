b899fc018b11b2d1cebd429d800ed96bd9fb5a4e
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-09-10 23:50:44 -0300

    feat: theme with figma colors and wrap the app with ThemeProvide

---
+++ web/src/theme/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+        common: { white: '#FFFFFF' },
+        primary: { main: '#144BC8', dark: '#001F66', contrastText: '#FFFFFF' },
+        secondary: { main: '#EE325D', contrastText: '#FFFFFF' },
+        text: { primary: '#121212', secondary: '#3D3D3D' },
+    },
+    typography: {
+        fontFamily: 'Inter, sans-serif',
+        h1: { fontFamily: 'Montserrat, sans-serif', fontWeight: 500 },
+        button: { textTransform: 'none' },
+    },
+});
