084a44a622c16b35cfb180b1b58fe09e1e858b1e
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-09-11 15:14:21 -0300

    feat: add footer with contact info

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,19 @@
+import { Box } from '@mui/material';
+import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import OffersList from './components/offers/OffersList';
 import OffersHeroBanner from './pages/OffersHeroBanner';
 
 function App() {
   return (
-    <>
-    <Header />
-    <OffersHeroBanner />
-    <OffersList />
-    </>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Header />
+      <Box component="main" sx={{ flexGrow: 1 }}>
+        <OffersHeroBanner />
+        <OffersList />
+      </Box>
+      <Footer />
+    </Box>
   )
 }
 
